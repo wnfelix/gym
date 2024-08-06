fb7b5be13ebfa678bac63bed31837075a3da9d9a
William Felix <william.nfelix@gmail.com>
2024-08-06 09:32:49 -0300

    expo update

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View, StatusBar } from "react-native";
-import { NativeBaseProvider } from "native-base";
+import { StatusBar } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
 import {
 	useFonts,
 	Roboto_400Regular,
 	Roboto_700Bold,
-} from "@expo-google-fonts/roboto";
-import { Loading } from "@components/Loading";
-import { THEME } from "src/theme";
-import { Routes } from "@routes/index";
-import { AuthContext, AuthContextProvider } from "@contexts/AuthContext";
+} from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
+import { THEME } from 'src/theme';
+import { Routes } from '@routes/index';
+import { AuthContextProvider } from '@contexts/AuthContext';
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
